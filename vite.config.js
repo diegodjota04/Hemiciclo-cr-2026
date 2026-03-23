6fb9d7bbb5f9bec691f73cc8bd1ddcb9bc55f163
diegodjota04 <38251274+diegodjota04@users.noreply.github.com>
2026-03-23 13:52:15 -0600

    Set base path for repository in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Añadimos esta línea con el nombre exacto de tu repositorio entre barras
+  base: '/Hemiciclo-cr-2026/', 
   plugins: [react()],
 })
